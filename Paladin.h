e64a6fabc58e289c4780e8ae8a2c3a7b5a7bff8a
AudibleP <64707877+AudibleP@users.noreply.github.com>
2020-12-04 20:52:41 -0600

    Update Paladin.h
    A01703259

--- Paladin.h
+++ Paladin.h
@@ -1,3 +1,13 @@
+/*
+Ricardo Nuñez Alanis
+A01703259
+Programacion orientada a objetos
+Proyecto final
+*/
+/*
+Clase Paladin, recibe elementos vacios de los stats y los llena
+Crea las acciones de los ataques y las guardias
+*/
 #ifndef PALADIN_H
 #define PALADIN_H
 
@@ -10,17 +20,17 @@
 class Paladin: public Rol
 {
 	public:
-		Paladin(string nam):Rol(nam,200,10,50,25){
+		Paladin(string nam):Rol(nam,200,10,50,25){//Recibe parametros vacios y da valores
 			
 		}
-		void Sword(Monster &m){
+		void Sword(Monster &m){//Accion de ataque con stat de atk
 			m.damage(getAttack()+30);
 		}
-		void Smite(Monster &m){
+		void Smite(Monster &m){//Accion de ataque con stat de mag
 			m.damage(getMagic()+30);
 		}
-		void Guard(Rol &c){
-			c.guarding(getDefense()+30);
+		void Guard(Rol &r){//Accion de defensa
+			r.guarding(getDefense());
 		}
 
 };
