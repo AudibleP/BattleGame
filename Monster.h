4abf7fd4a951d4f1bc89aab2265c0cb7da677c35
AudibleP <64707877+AudibleP@users.noreply.github.com>
2020-12-04 20:51:38 -0600

    Update Monster.h
    A01703259

--- Monster.h
+++ Monster.h
@@ -1,3 +1,13 @@
+/*
+Ricardo Nuñez Alanis
+A01703259
+Programacion orientada a objetos
+Proyecto final
+*/
+/*
+Clase monster le hereda ints y sitrings a sus respectivas clases
+Tambien tiene funciones para que el programa funcione
+*/
 #ifndef MONSTER_H
 #define MONSTER_H
 
@@ -11,18 +21,17 @@ class Monster{
 		int defense;
 		int magic;
 		
-		
 		string name;
 	
 	public:
-		Monster(string nam, int hp, int atk, int def, int mag){
+		Monster(string nam, int hp, int atk, int def, int mag){//constructor
 			name = nam;
 			health = hp;
 			attack = atk;
 			defense = def;
 			magic = mag;
 		}
-		
+		//getters
 		int getHealth (){
 			return health;
 		}
@@ -39,14 +48,13 @@ class Monster{
 			return name;
 		}
 		
-		void damage(int damage){
+		void damage(int damage){//Funcion para hacer el daño
 			health = health - damage;
 		}
-		void guarding (int guarding){
-			int damage;
-			guarding = guarding - damage;
-			health = health - guarding;
+		void guarding (int guarding){//Funcion para hacer defensa o recibir menos daño
+			health = health + guarding;
 		}
+		//setters
 		void setHealth(int h){
 			health = h;
 		}
