4e0c34a7aae9e70ecad4ffd07fd0039a1310dba5
AudibleP <64707877+AudibleP@users.noreply.github.com>
2020-12-04 20:50:12 -0600

    Update Hunter.h
    A01703259

--- Hunter.h
+++ Hunter.h
@@ -1,3 +1,13 @@
+/*
+Ricardo Nuñez Alanis
+A01703259
+Programacion orientada a objetos
+Proyecto final
+*/
+/*
+Clase Hunter, recibe elementos vacios de los stats y los llena
+Crea las acciones de los ataques y las guardias
+*/
 #ifndef HUNTER_H
 #define HUNTER_H
 
@@ -10,17 +20,17 @@
 class Hunter: public Rol
 {
 	public:
-		Hunter(string nam):Rol(nam,150,40,20,15){
+		Hunter(string nam):Rol(nam,150,40,30,25){//Recibe parametros vacios y da valores
 			
 		}
-		void Bow(Monster &m){
+		void Bow(Monster &m){//Accion de ataque con stat de atk
 			m.damage(getAttack()+30);
 		}
-		void Arcane_Shot(Monster &m){
+		void Arcane_Shot(Monster &m){//Accion de ataque con stat de mag
 			m.damage(getMagic()+30);
 		}
-		void Guard(Rol &c){
-			c.guarding(getDefense()+30);
+		void Guard(Rol &r){//Accion de defensa
+			r.guarding(getDefense());
 		}
 
 };
